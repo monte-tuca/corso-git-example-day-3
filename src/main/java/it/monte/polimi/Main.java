8a0b3fcf8c56381f22cb7be66ed352093deac15b
Francesco Montefusco <francesco.montefusco@mail.polimi.it>
2026-03-23 12:50:28 +0100

    feat: uses the Calculator to do math

--- src/main/java/it/monte/polimi/Main.java
+++ src/main/java/it/monte/polimi/Main.java
@@ -2,6 +2,8 @@ package it.monte.polimi;
 
 public class Main {
     public static void main(String[] args) {
+        Calculator c = new Calculator();
 
+        System.out.println(c.add(1,2));
     }
 }
